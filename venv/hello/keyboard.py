f1d52fe21c724a54c18d1c80b98eb850cff0fc39
Dmitry_Tyshkevich <tihon2008199213@gmail.com>
2023-05-14 20:38:47 +0300

    functional enhancement

--- venv/hello/keyboard.py
+++ venv/hello/keyboard.py
@@ -3,19 +3,34 @@ from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButt
     InlineKeyboardButton
 
 start = ReplyKeyboardMarkup(resize_keyboard=True)
-in_literature = InlineKeyboardMarkup()
-in_info = InlineKeyboardMarkup()
-buttons_models_audi = InlineKeyboardMarkup(row_width=2)
-
-info = KeyboardButton("Info")  # кнопка информации
-stats = KeyboardButton("Литература")
+info = KeyboardButton("Info")
+statistics = KeyboardButton('Статистика')
+lit = KeyboardButton("Литература")
 audi_class_a = KeyboardButton("AUDI Class A")
 restart = KeyboardButton('/restart')
+registration = KeyboardButton('Регистрация')
+registration_data = KeyboardButton('Данные пользователя')
+start.add(lit, audi_class_a)
+start.add(info, statistics)
+start.add(registration, registration_data)
+start.add(restart)
 
-reference_list = InlineKeyboardButton(text='Открыть список',
-                                      callback_data='referencelist')
+in_literature = InlineKeyboardMarkup()
+reference_list = InlineKeyboardButton(text='Открыть список', callback_data='referencelist')
+in_literature.add(reference_list)
+
+in_info = InlineKeyboardMarkup()
 inline_info = InlineKeyboardButton(text='Получить информацию', callback_data='inlineinfo')
+in_info.add(inline_info)
 
+in_stats = InlineKeyboardMarkup()
+in_stats.add(InlineKeyboardButton(f'Да', callback_data='join'))
+in_stats.add(InlineKeyboardButton(f'Нет', callback_data='cancle'))
+
+unregistration = InlineKeyboardMarkup()
+unregistration.add(InlineKeyboardButton('Удалить данные?', callback_data='delete'))
+
+buttons_models_audi = InlineKeyboardMarkup(row_width=2)
 audi_a1 = InlineKeyboardButton(text='AUDI A1', callback_data='AUDI_A1')
 audi_a2 = InlineKeyboardButton(text='AUDI A2', callback_data='AUDI_A2')
 audi_a3 = InlineKeyboardButton(text='AUDI A3', callback_data='AUDI_A3')
@@ -26,13 +41,6 @@ audi_a6 = InlineKeyboardButton(text='AUDI A6', callback_data='AUDI_A6')
 audi_a6_allroad = InlineKeyboardButton(text='AUDI A6-ALLROAD', callback_data='AUDI_A6-ALLROAD')
 audi_a7 = InlineKeyboardButton(text='AUDI A7', callback_data='AUDI_A7')
 audi_a8 = InlineKeyboardButton(text='AUDI A8', callback_data='AUDI_A8')
-
-start.add(stats, audi_class_a, info)
-start.add(restart)  # добавляем кнопуи в основу бота
-
-in_literature.add(reference_list)
-in_info.add(inline_info)
-
 buttons_models_audi.add(audi_a1, audi_a2)
 buttons_models_audi.add(audi_a3, audi_a4)
 buttons_models_audi.add(audi_a4_allroad, audi_a5)
--- venv/hello/main.py
+++ venv/hello/main.py
@@ -36,6 +36,66 @@ logging.basicConfig(
            u'[%(asctime)s] %(message)s')
 
 
+class User_registration(StatesGroup):
+    step_1 = State()
+    step_2 = State()
+    step_3 = State()
+    step_4 = State()
+
+
+@dp.message_handler(Text('Регистрация'), state=None)
+async def registr(message: types.Message, state: FSMContext):
+    with open('registration_data.json', encoding='utf-8') as f:
+        user_data = json.load(f)
+    if not str(message.chat.id) in user_data:
+        await message.answer('1. Введите имя:', reply_markup=ReplyKeyboardRemove())
+        await User_registration.step_1.set()
+    else:
+        await message.answer('Вы уже зарегистрированы!')
+        await state.finish()
+
+
+
+@dp.message_handler(state=User_registration.step_1)
+async def first_name_input(message: types.Message, state: FSMContext):
+    await state.update_data(first_name=message.text.capitalize())
+    await message.answer('Ваше имя сохранено!\n'
+                         '2. Введите фамилию: ')
+
+    await User_registration.step_2.set()
+
+
+@dp.message_handler(state=User_registration.step_2)
+async def last_name_input(message: types.Message, state: FSMContext):
+    await state.update_data(last_name=message.text.capitalize())
+    await message.answer('Ваша фамилия сохранена!\n'
+                         '3. Введите почтовый адрес: ')
+
+    await User_registration.step_3.set()
+
+
+@dp.message_handler(state=User_registration.step_3)
+async def mailing_address_input(message: types.Message, state: FSMContext):
+    await state.update_data(mailing_address=message.text)
+    await message.answer('Ваша почтовый адрес сохранен!\n'
+                         '4. Отправьте фото для Вашего профиля: ')
+
+    await User_registration.step_4.set()
+
+
+@dp.message_handler(content_types=types.ContentTypes.PHOTO, state=User_registration.step_4)
+async def load_photo(message: types.Message, state: FSMContext):
+    await state.update_data(photo=message.photo[0].file_id)
+    data = await state.get_data()
+    with open('registration_data.json', encoding='utf-8') as f:
+        user_data = json.load(f)
+    user_data[str(message.chat.id)] = data
+    with open('registration_data.json', 'w', encoding='utf-8') as file:
+        json.dump(user_data, file, indent=4, ensure_ascii=False)
+    await message.answer('Регистрация успешно завершена!', reply_markup=keyboard.start)
+    await state.finish()
+
+
 # Обработчик команд "/start" и "/restart"
 @dp.message_handler(Command(["start", "restart"]), state=None)  # задаем название команды start
 async def welcome(message):
@@ -52,11 +112,37 @@ async def welcome(message):
     # 2 способ вывода сообщения:
     await message.answer(f'Здравствуйте, {message.from_user.first_name}, Bot запущен',
                          reply_markup=keyboard.start)
+
     # после проверки и записи выводим сообщение с именем пользователя и отображаем кнопки
 
 
+@dp.message_handler(commands=['mailing_list'])
+async def mailing_list(message: types.Message):
+    if message.chat.id == config.admin:
+        await bot.send_message(message.chat.id, f'*Рассылка началась'
+                                                f'\nБот оповестит, когда закончит рассылку*',
+                               parse_mode=types.ParseMode.MARKDOWN_V2)
+        recieve_users, block_users = 0, 0
+        with open('user.txt') as file:
+            joined_users = set()
+            for line in file:
+                joined_users.add(line.strip())
+
+            for user in joined_users:
+                try:
+                    await bot.send_photo(user, open('carl.jpg', 'rb'))
+                    recieve_users += 1
+                except:
+                    block_users += 1
+                await asyncio.sleep(0.4)
+            await bot.send_message(message.chat.id, f'Рассылка завершена \n'
+                                                    f'Сообщение получили: *{recieve_users}* пользователей \n'
+                                                    f'Заблокировали бота *{block_users}*',
+                                   parse_mode=types.ParseMode.MARKDOWN_V2)
+
+
 # Обработчик команд с клавиатуры
-@dp.message_handler(Text(['Info', 'Литература', "AUDI Class A"]), state=None)
+@dp.message_handler(Text(['Info', 'Литература', "AUDI Class A", 'Статистика']), state=None)
 async def get_message(message: types.Message):
     match message.text:
         case 'Info':
@@ -67,7 +153,23 @@ async def get_message(message: types.Message):
         case "AUDI Class A":
             await bot.send_message(message.chat.id, text='Выберите модель:',
                                    reply_markup=keyboard.buttons_models_audi)
+        case 'Статистика':
+            await message.answer('Хочешь посмотреть статистику бота?', reply_markup=keyboard.in_stats)
 
+
+@dp.message_handler(Text(['Данные пользователя']))
+async def output_logging_data(message: types.Message):
+    with open('registration_data.json', encoding='utf-8') as f:
+        user_data = json.load(f)
+    id = str(message.chat.id)
+    if id in user_data:
+        await message.answer_photo(user_data[id]["photo"])
+        await message.answer(f'Имя: {user_data[id]["first_name"]}\n'
+                             f'Фамилия: {user_data[id]["last_name"]}\n'
+                             f'Email: {user_data[id]["mailing_address"]}', reply_markup=keyboard.unregistration)
+
+    else:
+        await message.answer('Ваш профиль не зарегистрирован!')
 
 # Обработчик инлайн кнопок 'Открыть список' и 'Получить информацию'
 @dp.callback_query_handler(text=['referencelist', 'inlineinfo'])
@@ -89,6 +191,33 @@ async def func(call: types.CallbackQuery):
         for link in parser.links_per_model[call.data]:
             await call.message.answer(link)
         await call.answer()
+
+
+@dp.callback_query_handler(text_contains='join')
+async def join(call: types.CallbackQuery):
+    if call.message.chat.id == config.admin:
+        d = sum(1 for line in open('user.txt'))
+        await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                    text=f'Ботом воспользовалось *{d}* человек', parse_mode='Markdown')
+    else:
+        await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                    text='У тебя нет админки\n Куда ты полез', parse_mode='Markdown')
+
+
+@dp.callback_query_handler(text_contains='cancle')
+async def cancle(call: types.CallbackQuery):
+    await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
+                                text='Ты вернулся в главное меню. Жми опять кнопки', parse_mode='Markdown')
+
+@dp.callback_query_handler(text_contains='delete')
+async def del_registration(call: types.CallbackQuery):
+    with open('registration_data.json', encoding='utf-8') as f:
+        user_data = json.load(f)
+    del user_data[str(call.message.chat.id)]
+    with open('registration_data.json', 'w', encoding='utf-8') as file:
+        json.dump(user_data, file, indent=4, ensure_ascii=False)
+    await call.message.answer('Ваши данные успешно удалены!')
+    await call.answer()
 
 
 if __name__ == '__main__':
