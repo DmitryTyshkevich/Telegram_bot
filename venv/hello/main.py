19cdbb122d7063ecc05cfb37ab025bd5f7a26dac
Dmitry_Tyshkevich <tihon2008199213@gmail.com>
2023-05-19 14:48:42 +0300

    changing the database on sqlite3

--- venv/hello/main.py
+++ venv/hello/main.py
@@ -17,9 +17,10 @@ import keyboard  # импортируем файл config keyboard
 import logging  # модуль для вывода информации
 import json
 import parser
+import sqlite3
 
-
-asyncio.set_event_loop(asyncio.new_event_loop())  # Устанавливает loop как текущий цикл событий для текущего потока ОС.(для запуска
+asyncio.set_event_loop(
+    asyncio.new_event_loop())  # Устанавливает loop как текущий цикл событий для текущего потока ОС.(для запуска
 # модуля с парсером)
 
 storage = MemoryStorage()  # FSM
@@ -45,15 +46,25 @@ class User_registration(StatesGroup):
 
 @dp.message_handler(Text('Регистрация'), state=None)
 async def registr(message: types.Message, state: FSMContext):
-    with open('registration_data.json', encoding='utf-8') as f:
-        user_data = json.load(f)
-    if not str(message.chat.id) in user_data:
+    id = message.chat.id
+    conn = sqlite3.connect('registration_db.db')
+    cur = conn.cursor()
+    cur.execute("""CREATE TABLE IF NOT EXISTS users(
+        user_id INT PRIMARY KEY,
+        first_name,
+        last_name,
+        email,
+        photo);
+        """)
+    conn.commit()
+    id_db = cur.execute("SELECT user_id FROM users WHERE user_id == ?", (id,)).fetchone()
+    if not id_db:
         await message.answer('1. Введите имя:', reply_markup=ReplyKeyboardRemove())
         await User_registration.step_1.set()
     else:
         await message.answer('Вы уже зарегистрированы!')
         await state.finish()
-
+    conn.close()
 
 
 @dp.message_handler(state=User_registration.step_1)
@@ -87,13 +98,17 @@ async def mailing_address_input(message: types.Message, state: FSMContext):
 async def load_photo(message: types.Message, state: FSMContext):
     await state.update_data(photo=message.photo[0].file_id)
     data = await state.get_data()
-    with open('registration_data.json', encoding='utf-8') as f:
-        user_data = json.load(f)
-    user_data[str(message.chat.id)] = data
-    with open('registration_data.json', 'w', encoding='utf-8') as file:
-        json.dump(user_data, file, indent=4, ensure_ascii=False)
+    tuple_data = (message.chat.id, data['first_name'], data['last_name'], data['mailing_address'], data['photo'])
+
+    conn = sqlite3.connect('registration_db.db')
+    cur = conn.cursor()
+
+    cur.execute("INSERT INTO users VALUES(?, ?, ?, ?, ?);", tuple_data)
+    conn.commit()
+    conn.close()
     await message.answer('Регистрация успешно завершена!', reply_markup=keyboard.start)
     await state.finish()
+
 
 @dp.message_handler(state=User_registration.step_4)
 async def none_photo(message: types.Message, state: FSMContext):
@@ -116,7 +131,6 @@ async def welcome(message):
     # 2 способ вывода сообщения:
     await message.answer(f'Здравствуйте, {message.from_user.first_name}, Bot запущен',
                          reply_markup=keyboard.start)
-
 
     # после проверки и записи выводим сообщение с именем пользователя и отображаем кнопки
 
@@ -148,7 +162,7 @@ async def mailing_list(message: types.Message):
 
 # Обработчик команд с клавиатуры
 @dp.message_handler(Text(['Info', 'Литература', "AUDI Class A", 'Статистика',
-                          'Покажи пользователя', 'Отправить фото', 'Загрузить фото']), state=None)
+                          'ID пользователя', 'Отправить фото', 'Загрузить фото']), state=None)
 async def get_message(message: types.Message):
     match message.text:
         case 'Info':
@@ -161,29 +175,29 @@ async def get_message(message: types.Message):
                                    reply_markup=keyboard.buttons_models_audi)
         case 'Статистика':
             await message.answer('Хочешь посмотреть статистику бота?', reply_markup=keyboard.in_stats)
-        case 'Покажи пользователя':
-            await message.answer('...',reply_markup=keyboard.inl_show_user)
+        case 'ID пользователя':
+            await message.answer('...', reply_markup=keyboard.inl_show_user)
         case 'Отправить фото':
             await bot.send_photo(message.chat.id, open('carl.jpg', 'rb'))
         case 'Загрузить фото':
             await message.answer('Отправьте пожалуйста фото')
 
 
-
-
 @dp.message_handler(Text(['Данные пользователя']))
 async def output_logging_data(message: types.Message):
-    with open('registration_data.json', encoding='utf-8') as f:
-        user_data = json.load(f)
-    id = str(message.chat.id)
-    if id in user_data:
-        await message.answer_photo(user_data[id]["photo"])
-        await message.answer(f'Имя: {user_data[id]["first_name"]}\n'
-                             f'Фамилия: {user_data[id]["last_name"]}\n'
-                             f'Email: {user_data[id]["mailing_address"]}', reply_markup=keyboard.unregistration)
-
+    id = message.chat.id
+    conn = sqlite3.connect('registration_db.db')
+    cur = conn.cursor()
+    data_user = cur.execute("SELECT * FROM users WHERE user_id == ?", (id,)).fetchone()
+    if data_user:
+        await message.answer_photo(data_user[-1])
+        await message.answer(f'Имя: {data_user[1]}\n'
+                             f'Фамилия: {data_user[2]}\n'
+                             f'Email: {data_user[3]}', reply_markup=keyboard.unregistration)
     else:
         await message.answer('Ваш профиль не зарегистрирован!')
+    conn.close()
+
 
 # Обработчик инлайн кнопок 'Открыть список' и 'Получить информацию'
 @dp.callback_query_handler(text=['referencelist', 'inlineinfo'])
@@ -223,25 +237,30 @@ async def cancle(call: types.CallbackQuery):
     await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                 text='Ты вернулся в главное меню. Жми опять кнопки', parse_mode='Markdown')
 
+
 @dp.callback_query_handler(text_contains='delete')
 async def del_registration(call: types.CallbackQuery):
-    with open('registration_data.json', encoding='utf-8') as f:
-        user_data = json.load(f)
-    del user_data[str(call.message.chat.id)]
-    with open('registration_data.json', 'w', encoding='utf-8') as file:
-        json.dump(user_data, file, indent=4, ensure_ascii=False)
+    id = call.message.chat.id
+    conn = sqlite3.connect('registration_db.db')
+    cur = conn.cursor()
+    cur.execute("DELETE FROM users WHERE user_id == ?", (id,))
     await call.message.answer('Ваши данные успешно удалены!')
     await call.answer()
+    conn.commit()
+    conn.close()
+
 
 @dp.callback_query_handler(text_contains='show_id')
 async def print_id(call: types.CallbackQuery):
     await call.message.answer(f'Ваш id: {call.message.chat.id}')
     await call.answer()
 
+
 @dp.callback_query_handler(text_contains='back')
 async def cancel(call: types.CallbackQuery):
     await bot.edit_message_text(chat_id=call.message.chat.id, message_id=call.message.message_id,
                                 text='Вы отменили выбор', parse_mode='Markdown')
+
 
 @dp.message_handler(content_types=types.ContentTypes.PHOTO)
 async def load_photo(message: types.Message):
@@ -250,9 +269,11 @@ async def load_photo(message: types.Message):
     await message.answer('Фото успешно загружено')
     await message.answer_photo(message.photo[0].file_id)
 
+
 @dp.message_handler(content_types=["location"])
 def check_location(message: types.Message):
     bot.send_message(message.chat.id, message.location.latitude, message.location.longitude)
+
 
 if __name__ == '__main__':
     print('Бот запущен!')
